1adfc085ce00876383caf1ff3466a3b8d6507bb9
gdemare <44768462+gdemare@users.noreply.github.com>
2022-10-30 12:40:56 +0100

    a

---
+++ serie-renommage.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import os
+import glob
+import re
+from tkinter import Tk
+from tkinter.filedialog import askdirectory
+
+
+# In[ ]:
+
+
+dossier = askdirectory(title='Select Folder')
+serie = re.search(r'((?!(/|\\)).)*$' , dossier).group(0)
+os.chdir(dossier)
+for i in os.listdir(dossier):
+    if "mkv" in i:
+        episode = re.search( "[S|s][0-9]{2}[E|e][0-9]{2}", i ).group(0)
+        nvnom = serie + episode + i[-4:]
+        print(i)
+        print(nvnom)
+        os.rename(i, nvnom)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
